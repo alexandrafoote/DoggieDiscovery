59b12d0abd19beda52f2192c8737e728009dbd1a
Nigel Davis <nigeledavis@ufl.edu>
2018-07-25 11:27:10 -0700

    added functionality for camera

--- BaseFiles/src/pages/about/about.ts
+++ BaseFiles/src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-about',
@@ -7,12 +8,31 @@ import { NavController, LoadingController, NavParams } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController,  public navParams: NavParams, public loadingCtrl: LoadingController)
+  myPhoto:any;
+
+  constructor(public navCtrl: NavController,  private camera: Camera, public navParams: NavParams, public loadingCtrl: LoadingController)
   {
     console.log(navParams.get('val'));
   }
 
+    takePhoto() {
+      
+      const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.FILE_URI,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+      }
+      
+      this.camera.getPicture(options).then((imageData) => {
+       // imageData is either a base64 encoded string or a file URI
+       // If it's base64 (DATA_URL):
+       this.myPhoto = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+       // Handle error
+      });
 
+    }
     presentLoading() {
       this.loadingCtrl.create({
         content: 'Please wait...',
